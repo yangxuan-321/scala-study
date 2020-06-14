23a95ae0f581186030f3864aa601eaa82b467755
MODA-Master <yangxuan_321@163.com>
2020-06-14 23:23:06 +0800

    合并代码

--- src/main/java/org/moda/zzkj/OperCharOverloadOps.scala
+++ src/main/java/org/moda/zzkj/OperCharOverloadOps.scala
@@ -1,6 +1,11 @@
 package org.moda.zzkj
 
+import scala.beans.BeanProperty
+
 class Dog(val name: String, val sex: String){
+  @BeanProperty
+  var test: String = _
+
   def +(dog: Dog): Dog ={
    new Dog(s"${name} <-> ${dog.name}", s"${sex} <-> ${dog.sex}")
   }
@@ -14,6 +19,9 @@ class Dog(val name: String, val sex: String){
 object OperCharOverloadOps {
   def main(args: Array[String]): Unit = {
     val baby = new Dog("jams", "boy") + new Dog("kiki", "girl")
-    println(baby)
+    println(baby.name)
+    println(baby.test)
+    println(baby.getTest)
   }
 }
+,

b9488a278e27dca76e8d367ab5de1db5c4395742
fubeixian <fubx@chinamobiad.com>
2020-06-15 09:00:03 +0800

    1.操作符重载练习 2.字符串函数插值练习

--- src/main/java/org/moda/OperCharOverloadOps.scala
+++ src/main/java/org/moda/OperCharOverloadOps.scala
@@ -1,11 +1,15 @@
 package org.moda
 
-class Dog(val name: String, val sex: String){
+class Dog(val name: String, var sex: String){
   def +(dog: Dog): Dog ={
    new Dog(s"${name} <-> ${dog.name}", s"${sex} <-> ${dog.sex}")
   }
 
   override def toString: String = {s"name: ${name}, sex: ${sex}"}
+}
+
+object Dog{
+  def apply(name: String, sex: String): Dog = new Dog(name, sex)
 }
 
 /**
@@ -15,5 +19,9 @@ object OperCharOverloadOps {
   def main(args: Array[String]): Unit = {
     val baby = new Dog("jams", "boy") + new Dog("kiki", "girl")
     println(baby)
+
+    // 不写 new 是因为 其默认 调用了 object Dog 的apply方法
+    val baby1 = Dog("hello", "boy") + Dog("world", "girl") + Dog("!", "NONE")
+    println(baby1)
   }
 }

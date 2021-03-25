c22d510fb3e78106135f0e8f4302fc91ae3a8251
yangxuan_321 <yangxuan_321@163.com>
2021-03-25 23:36:25 +0800

    语法解析器组合子设计学习

--- src/main/java/org/moda/scala函数式编程/C9.scala
+++ src/main/java/org/moda/scala函数式编程/C9.scala
@@ -4,5 +4,17 @@ package org.moda.scala函数式编程
  * 语法分析器组合子
  */
 class C9 {
+}
 
+trait Parsers[ParserError, Parser[+_]] { self =>
+  def char(c: Char): Parser[Char]
+  def run[A](p: Parser[A])(input: String): Either[ParseError, A]
+  // 此处将 string 申明为隐式转换
+  implicit def string(s: String): Parser[String]
+  implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
+  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+  case class ParserOps[A](p: Parser[A]) {
+    def |[B>:A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+    def or[B>:A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+  }
 }

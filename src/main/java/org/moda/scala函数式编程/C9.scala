51c5a3b31fdfbdb7565d4df795cb71a9f22247ae
yangxuan_321 <yangx@chinamobiad.com>
2021-03-26 20:26:26 +0800

    material find

--- src/main/java/org/moda/scala函数式编程/C9.scala
+++ src/main/java/org/moda/scala函数式编程/C9.scala
@@ -1,20 +1,31 @@
-package org.moda.scala函数式编程
-
-/**
- * 语法分析器组合子
- */
-class C9 {
-}
-
-trait Parsers[ParserError, Parser[+_]] { self =>
-  def char(c: Char): Parser[Char]
-  def run[A](p: Parser[A])(input: String): Either[ParseError, A]
-  // 此处将 string 申明为隐式转换
-  implicit def string(s: String): Parser[String]
-  implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
-  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
-  case class ParserOps[A](p: Parser[A]) {
-    def |[B>:A](p2: Parser[B]): Parser[B] = self.or(p, p2)
-    def or[B>:A](p2: Parser[B]): Parser[B] = self.or(p, p2)
-  }
-}
+//package org.moda.scala函数式编程
+//
+///**
+// * 语法分析器组合子
+// */
+//class C9 {
+//}
+//
+//trait Parsers[ParserError, Parser[+_]] { self =>
+//  def char(c: Char): Parser[Char]
+//  def run[A](p: Parser[A])(input: String): Either[ParserError, A]
+//  // 此处将 string 申明为隐式转换
+//  implicit def string(s: String): Parser[String]
+//  implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
+//  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
+//  // 是否 满足
+//  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+//  // 识别 一个东西(p)是不是 重复了 n 次
+//  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
+//  // 识别 一个东西(p)重复 并返回重复结果
+//  def many[A](p: Parser[A]): Parser[List[A]]
+//  def map[A, B](a: Parser[A])(f: A => B): Parser[B]
+//  case class ParserOps[A](p: Parser[A]) {
+//    def |[B>:A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+//    def or[B>:A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+//  }
+//  object Laws {
+//    def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop = forAll(in)(s => run(p1)(s) == run(p2)(s))
+//  }
+//}
+//
---
+++ src/main/java/org/moda/工作工具/Json.scala
@@ -0,0 +1,24 @@
+package org.moda.工作工具
+
+import java.io.File
+
+import scala.io.Source
+
+object Json1 {
+  def main(args: Array[String]): Unit = {
+    val c = Source.fromFile(new File("/opt/MODA/MODA-CODE/my-code/scala-study/src/main/java/org/moda/工作工具/1.json").getPath)
+      .getLines().toList.filter(_.contains("signature")).filterNot(_.contains(""""signature":"","""))
+     // c.map(cx => println(cx))
+    // println(c.size)
+    val cc = c.map(_.trim).map(_.replace(""""signature":"""", "")).map(_.replace("""",""", ""))
+    // cc.map(cx => println(cx))
+
+    val d = Source.fromFile(new File("/opt/MODA/MODA-CODE/my-code/scala-study/src/main/java/org/moda/工作工具/2.txt").getPath)
+      .getLines().toList
+      .filter(_.contains("OceanengineMaterialService"))
+      .filter(_.contains("result: Right("))
+
+    val xx = cc.filterNot(ccx => d.exists(_.contains(ccx)))
+    xx.map(println)
+  }
+}

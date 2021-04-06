04983ec8b07f59357f1a272d208dc62b55edc704
yangxuan_321 <yangxuan_321@163.com>
2021-04-06 23:58:39 +0800

    applicative学习

--- src/main/scala/org/moda/fp/applicative/C12.scala
+++ src/main/scala/org/moda/fp/applicative/C12.scala
@@ -45,6 +45,13 @@ trait Applicative1[F[_]] extends Functor[F] {
     // apply(value)(fb)
     apply(map(fa)(f.curried))(fb)
   }
+
+  def map3[A, B, C, D](fa: F[A], fb: F[B], fc: F[C])(f: (A, B, C) => D): F[D] =
+    apply(apply(apply(unit(f.curried))(fa))(fb))(fc)
+
+  def map4[A, B, C, D, E](fa: F[A], fb: F[B],
+                          fc: F[C], fd: F[D])(f: (A, B, C, D) => E): F[E] =
+    apply(apply(apply(apply(unit(f.curried))(fa))(fb))(fc))(fd)
 }
 
 object C12 {
